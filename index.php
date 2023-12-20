59d4393260576dcf56d16f4538df73fd12925402
Koen Eelen <koen.eelenpubliq.be>
2023-12-20 21:24:48 +0100

    start

--- index.php
+++ index.php
@@ -2,3 +2,9 @@
 
 $pdo = new Pdo('mysql:host=localhost;dbname=cvo', 'root', 'root');
 
+$result = $pdo->query('select * from cars order by name');
+$cars = $result->fetchAll();
+foreach($cars as $car) {
+    echo '<h2>'. $car['name'] .'</h2>';
+    echo '<p>'. $car['year'] .'</p>';
+}
