c0100ba750ec08a1c4fb09411887959bd2826e7e
Koen Eelen <koen.eelenpubliq.be>
2023-12-20 21:33:19 +0100

    prepare the prepare...

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-
 $pdo = new Pdo('mysql:host=localhost;dbname=cvo', 'root', 'root');
 
-$result = $pdo->query('select * from cars order by name');
+$result = $pdo->query('select * from cars where year = '. $_GET['year'] ?? 2010 .' order by name');
 $cars = $result->fetchAll();
 foreach($cars as $car) {
     echo '<h2>'. $car['name'] .'</h2>';
